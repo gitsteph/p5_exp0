0c9273d1da1a66a70d6876d3c7074673560b1a73
Stephanie H. Andrews <byronic.string@gmail.com>
2018-08-05 23:05:13 -0700

    tabs => spaces :X

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0, 300, 300, 100);
+    createCanvas(windowWidth, windowHeight);
+    background(0, 300, 300, 100);
 }
 
 function draw() {
-	ellipse(50, 50, 80, 80);
-	if (mouseIsPressed) {
-		fill(100, 200, 100, 100);
-	} else {
-		fill(0, 100, 0, 50);
-	}
-	ellipse(mouseX, mouseY, 80, 80);
+    ellipse(50, 50, 80, 80);
+    if (mouseIsPressed) {
+        fill(100, 200, 100, 100);
+    } else {
+        fill(0, 100, 0, 50);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
 }
